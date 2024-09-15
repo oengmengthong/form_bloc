0928b375ed5b67aa9d9c7191ff387f248b970f94
Enzo Santos <enzo.santos29@hotmail.com>
2024-09-15 10:45:47 -0300

    [flutter_form_bloc] refactor: replace deprecated WillPopScope with PopScope on example

--- packages/flutter_form_bloc/example/lib/main.dart
+++ packages/flutter_form_bloc/example/lib/main.dart
@@ -336,8 +336,8 @@ class LoadingDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
+    return PopScope(
+      canPop: false,
       child: Center(
         child: Card(
           child: Container(
