40a8bad28a0874b80ee63dd1832b037501e93802
Oeng Mengthong <mengthongoenggl@gmail.com>
2025-06-23 10:17:19 +0700

    upgrade to support flutter 3.32.4

--- form_bloc_web/lib/widgets/app_drawer.dart
+++ form_bloc_web/lib/widgets/app_drawer.dart
@@ -155,7 +155,7 @@ class _AppDrawerState extends State<AppDrawer> with RouteAware {
       alignment: Alignment.centerLeft,
       child: Text(
         'Examples',
-        style: Theme.of(context).textTheme.headline6,
+        style: Theme.of(context).textTheme.titleLarge,
       ),
     );
   }
